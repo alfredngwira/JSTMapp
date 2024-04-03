edeedcdf25d0945ae0bb2be9572640bb9c978229
alfredngwira <165509014+alfredngwira@users.noreply.github.com>
2024-04-03 00:00:44 -0700

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,1 +1,14 @@
+# run_app.R
+#
+#' @title A function to run the app
+#' @description Runs the JSTMapp Shiny web application.
+#' @details The function outputs the app for the user to supply the data and analysis the data
+#' @export
+run_app <- function() {
+  appDir <- system.file('JSTMapp', package='JSTMapp')
+  if (appDir == "") {
+    stop("Could not find MBGapp. Try re-installing `mypackage`.", call. = FALSE)
+  }
 
+  shiny::runApp(appDir, display.mode = "normal")
+}
