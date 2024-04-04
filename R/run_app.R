e1f1ab7790c333ed2d4e068b2274b93c83b387dc
alfredngwira <165509014+alfredngwira@users.noreply.github.com>
2024-04-04 00:58:03 -0700

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -7,7 +7,7 @@
 run_app <- function() {
   appDir <- system.file('JSTMapp', package='JSTMapp')
   if (appDir == "") {
-    stop("Could not find MBGapp. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find JSTMapp. Try re-installing `mypackage`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
