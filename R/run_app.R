1d093c35c022c88571dbe4ebabd55bb4e724b518
alfredngwira <165509014+alfredngwira@users.noreply.github.com>
2024-07-29 05:01:34 -0700

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,14 +1,5 @@
-# run_app.R
-#
-#' @title A function to run the app
-#' @description Runs the JSTMapp Shiny web application.
-#' @details The function outputs the app for the user to supply the data and analysis the data
+#' Runs the Shiny web application.
 #' @export
 run_app <- function() {
-  appDir <- system.file('JSTMapp', package='JSTMapp')
-  if (appDir == "") {
-    stop("Could not find JSTMapp. Try re-installing `mypackage`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(system.file('JSTMapp', package='JSTMapp'))
 }
